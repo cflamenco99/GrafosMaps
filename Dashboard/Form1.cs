85779b5335a8796b3f9d20724e202da945a52a8b
Carlos Flamenco <cflamenco99@gmail.com>
2021-12-09 17:09:41 -0600

    Pantalla base

--- Dashboard/Form1.cs
+++ Dashboard/Form1.cs
@@ -13,20 +13,9 @@ namespace Dashboard
 {
     public partial class Form1 : Form
     {
-
+        #region Metodos Generales
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
-
-        private static extern IntPtr CreateRoundRectRgn
-         (
-               int nLeftRect,
-               int nTopRect,
-               int nRightRect,
-               int nBottomRect,
-               int nWidthEllipse,
-               int nHeightEllipse
-
-         );
-
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
         public Form1()
         {
             InitializeComponent();
@@ -36,12 +25,10 @@ namespace Dashboard
             pnlNav.Left = btnDashbord.Left;
             btnDashbord.BackColor = Color.FromArgb(46, 51, 73);
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
         private void btnDashbord_Click(object sender, EventArgs e)
         {
             pnlNav.Height = btnDashbord.Height;
@@ -49,84 +36,18 @@ namespace Dashboard
             pnlNav.Left = btnDashbord.Left;
             btnDashbord.BackColor = Color.FromArgb(46, 51, 73);
         }
-
-        private void btnAnalytics_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnAnalytics.Height;
-            pnlNav.Top = btnAnalytics.Top;
-            btnAnalytics.BackColor = Color.FromArgb(46, 51, 73);
-        }
-
-        private void btnCalender_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnCalender.Height;
-            pnlNav.Top = btnCalender.Top;
-            btnCalender.BackColor = Color.FromArgb(46, 51, 73);
-
-        }
-
-        private void btnContactUs_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnContactUs.Height;
-            pnlNav.Top = btnContactUs.Top;
-            btnContactUs.BackColor = Color.FromArgb(46, 51, 73);
-        }
-
-        private void btnsettings_Click(object sender, EventArgs e)
-        {
-            pnlNav.Height = btnsettings.Height;
-            pnlNav.Top = btnsettings.Top;
-            btnsettings.BackColor = Color.FromArgb(46, 51, 73);
-        }
-
         private void btnDashbord_Leave(object sender, EventArgs e)
         {
             btnDashbord.BackColor = Color.FromArgb(24, 30, 54);
         }
-
-        private void btnAnalytics_Leave(object sender, EventArgs e)
-        {
-            btnAnalytics.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnCalender_Leave(object sender, EventArgs e)
-        {
-            btnCalender.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnContactUs_Leave(object sender, EventArgs e)
-        {
-            btnContactUs.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
-        private void btnsettings_Leave(object sender, EventArgs e)
-        {
-            btnsettings.BackColor = Color.FromArgb(24, 30, 54);
-        }
-
         private void button1_Click_1(object sender, EventArgs e)
         {
             Application.Exit();
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            textBox1.Clear();
-        }
-
-        private void textBox1_Leave(object sender, EventArgs e)
-        {
-
-        }
-
+        }     
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
         }
+        #endregion
     }
 }
