3a4c2b3059d2ef21452e21b5ce359f10863386a5
Carlos Flamenco <cflamenco99@gmail.com>
2021-12-11 00:13:32 -0600

    Algoritmo base

--- Dashboard/Form1.Designer.cs
+++ Dashboard/Form1.Designer.cs
@@ -49,7 +49,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(1062, 5);
+            this.button1.Location = new System.Drawing.Point(1058, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(25, 25);
             this.button1.TabIndex = 12;
--- Dashboard/Form1.cs
+++ Dashboard/Form1.cs
@@ -24,6 +24,33 @@ namespace Dashboard
             pnlNav.Top = btnDashbord.Top;
             pnlNav.Left = btnDashbord.Left;
             btnDashbord.BackColor = Color.FromArgb(46, 51, 73);
+
+            //Ejemplo de uso de algoritmo
+            Node NodeA = new Node() { City = "A" };
+            Node NodeB = new Node() { City = "B" };
+            Node NodeC = new Node() { City = "C" };
+            Node NodeD = new Node() { City = "D" };
+
+            NodeA.Ways.Add(new Way() { Node = NodeB, Distance = 5 });
+            NodeA.Ways.Add(new Way() { Node = NodeC, Distance = 15 });
+            NodeA.Ways.Add(new Way() { Node = NodeD, Distance = 7 });
+            NodeB.Ways.Add(new Way() { Node = NodeA, Distance = 5 });
+            NodeB.Ways.Add(new Way() { Node = NodeC, Distance = 10 });
+            NodeB.Ways.Add(new Way() { Node = NodeD, Distance = 5 });
+            NodeC.Ways.Add(new Way() { Node = NodeA, Distance = 15 });
+            NodeC.Ways.Add(new Way() { Node = NodeB, Distance = 10 });
+            NodeC.Ways.Add(new Way() { Node = NodeD, Distance = 3 });
+            NodeD.Ways.Add(new Way() { Node = NodeA, Distance = 7 });
+            NodeD.Ways.Add(new Way() { Node = NodeB, Distance = 5 });
+            NodeD.Ways.Add(new Way() { Node = NodeC, Distance = 3 });
+
+            List<Node> graph = new List<Node>() {
+                NodeA, NodeB, NodeC, NodeD
+            };
+
+            var algorithm = new Algorithm(graph, 3, NodeA);
+            algorithm.Run();
+            MessageBox.Show(algorithm.GetAllRoutes);
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -47,6 +74,110 @@ namespace Dashboard
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+        #endregion
+
+        #region Algoritmo Grafos Maps
+        public class Algorithm
+        {
+            private List<Node> _graph { get; set; }
+            private int _n { get; set; }
+            private Node _origin { get; set; }
+            private List<Route> _solutions { get; set; }
+
+            public string GetAllRoutes
+            {
+                get
+                {
+                    string result = "";
+                    foreach (var route in _solutions)
+                    {
+                        foreach (var node in route.Nodes)
+                        {
+                            result += node.City + ", ";
+                        }
+                        result += " " + route.TotalDistance + "\n";
+                    }
+
+                    return result;
+                }
+            }
+
+            public Algorithm(List<Node> graph, int n, Node origin)
+            {
+                _graph = graph;
+                _n = n;
+                _origin = origin;
+            }
+
+            public void Run()
+            {
+                _solutions = new List<Route>();
+                for (int i = 0; i < _n; i++)
+                {
+                    _solutions.Add(Generate());
+                }
+                _solutions = _solutions.OrderBy(d => d.TotalDistance).ToList();
+            }
+
+            public Route Generate()
+            {
+                var solution = new Route();
+                solution.Nodes.Add(_origin);
+                Node current = _origin;
+                for (int i = 0; i < _graph.Count - 1; i++)
+                {
+                    Node next = null;
+                    do
+                    {
+                        next = NextNode(current);
+                    } while (solution.Nodes.Contains(next));
+
+                    solution.Nodes.Add(next);
+                    solution.TotalDistance += current.Ways.Where(d => d.Node.City == next.City).First().Distance;
+
+                    current = next;
+                }
+
+                solution.Nodes.Add(_origin);
+                solution.TotalDistance += current.Ways.Where(d => d.Node.City == _origin.City).First().Distance;
+                return solution;
+            }
+
+            private Node NextNode(Node current)
+            {
+                int nextNode = new Random().Next(0, _graph.Count - 1);
+                return current.Ways[nextNode].Node;
+            }
+        }
+
+        public class Node
+        {
+            public string City { get; set; }
+            public List<Way> Ways { get; set; }
+
+            public Node()
+            {
+                Ways = new List<Way>();
+            }
+        }
+
+        public struct Way
+        {
+            public Node Node { get; set; }
+            public int Distance { get; set; }
+        }
+
+        public class Route
+        {
+            public List<Node> Nodes { get; set; }
+            public int TotalDistance { get; set; }
+
+            public Route()
+            {
+                Nodes = new List<Node>();
+                TotalDistance = 0;
+            }
         }
         #endregion
     }
